915028cf0b5c30fa101ada88aba5249297657ff9
Sherv31 <sherwynbraganza@gmail.com>
2017-07-06 23:11:31 -0600

    Just Updates
    Updates

---
+++ Android/Planner/app/src/main/java/com/gts/planner/controller/AddCourseActivity.java
@@ -0,0 +1,10 @@
+package com.gts.planner.controller;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 7408588 on 7/6/2017.
+ */
+
+public class AddCourseActivity extends AppCompatActivity {
+}
