68af8bc8aa82c7b50e3cace086620daa2b62b58a
Sherv31 <sherwynbraganza@gmail.com>
2017-08-04 13:36:53 -0600

    Add Course Activity Complete
    Except for SQL part. DayPicker dialog fully complete

--- Android/Planner/app/src/main/java/com/gts/planner/controller/AddCourseActivity.java
+++ Android/Planner/app/src/main/java/com/gts/planner/controller/AddCourseActivity.java
@@ -1,4 +1,6 @@
  package com.gts.planner.controller;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,12 +14,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TimePicker;
 
 import com.gts.planner.App;
 import com.gts.planner.R;
-import com.gts.planner.infra.DatePicker;
 import com.gts.planner.infra.DayPicker;
+import com.gts.planner.infra.TimePicker;
 import com.gts.planner.model.Course;
 import com.gts.planner.model.Task;
 
@@ -31,7 +32,8 @@ import java.util.Locale;
 
 //assuming one class per day only
 
-public class AddCourseActivity extends AppCompatActivity  {
+public class AddCourseActivity extends AppCompatActivity implements TimePicker.TimeSelectListener,
+        DayPicker.DayPickerListener {
 
     private Course NewCourse = new Course(); // creating a new class of objects for a new course.
     private static long count = 1; // Meant to serve as the id but can be deleted if already
@@ -39,18 +41,22 @@ public class AddCourseActivity extends AppCompatActivity  {
     private Button save_button; // button that transfers input ot Database
     private EditText title; // Title of the Course
     private Button day; // Due day of the task
-    private TimePicker sTime; //Start time of the course
+    private Button sTime; //Start time of the course
     private Button eTime; //End time of the course
     private EditText desc; // Notes pertaining to the task
     private EditText prof; // course professor name
     private EditText location; // course location
-    private Date CourseDayTime; // variable that stores the day-time data of a course
+    private Date CourseStartTime = new Date(); // variable that sstores the start time data
+    private Date CourseEndTime = new Date(); // variable that stores the end tme data
+    int passcode; // the passcode that controls the created <code>if<code> statements pertaining to
+                  // the required process
 
-    private Bundle args = new Bundle();
+    private Bundle args = new Bundle(); // a heap created to store data to be passed into the dialogs
 
-    private ArrayList mSelectedDays = new ArrayList();
+    private ArrayList mSelectedDays = new ArrayList(); // variable created to store the data of the
+                                                       // days the courses are on
 
-    private SimpleDateFormat formatter_time = new SimpleDateFormat("hh:mm aa", Locale.getDefault());
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,32 +79,37 @@ public class AddCourseActivity extends AppCompatActivity  {
                         args.putStringArrayList("days", mSelectedDays);
                         day_picker.setArguments(args);
                         day_picker.show(getFragmentManager(), "dayPicker");
-                        try {
-                            mSelectedDays = args.getStringArrayList("days");
-                        }catch (NullPointerException e){
-                            mSelectedDays = new ArrayList();
-                        }
+                    }
+                });
+
+
+        sTime = (Button) findViewById(R.id.sTime_course);
+        sTime.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TimePicker tPicker = new TimePicker();
+                        args.putString("sel_time", timeFormat.format(CourseStartTime));
+                        tPicker.setArguments(args);
+                        tPicker.show(getFragmentManager(), "time_picker");
+                        passcode = 1;
                     }
                 }
         );
 
-
-<<<<<<< HEAD
-        sTime = (Button) findViewById(R.id.sTime_course);
-=======
-        sTime = (TimePicker) findViewById(R.id.sTime_course);
-        Integer hour, minutes;
-        if (Build.VERSION.SDK_INT >= 23 ) {
-            hour = sTime.getHour();
-            minutes = sTime.getMinute();
-        } else {
-            hour = sTime.getCurrentHour();
-            minutes = sTime.getCurrentMinute();
-        }
-        final Time time = new Time(hour, minutes, 0);
-        System.out.println(mSelectedDays);
->>>>>>> 00968614b64eb62890235fef5761f33d953a4b46
         eTime = (Button) findViewById(R.id.eTime_course);
+        eTime.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TimePicker tPicker = new TimePicker();
+                        args.putString("sel_time", timeFormat.format(CourseEndTime));
+                        tPicker.setArguments(args);
+                        tPicker.show(getFragmentManager(), "time_picker");
+                        passcode = 2;
+                    }
+                }
+        );
         save_button = (Button) findViewById(R.id.save_course);
         save_button.setOnClickListener(
                 new View.OnClickListener() {
@@ -112,7 +123,6 @@ public class AddCourseActivity extends AppCompatActivity  {
 
                         //converts the date object into epoch format(getTime function)
                         NewCourse.setDay(mSelectedDays.toString());
-                        NewCourse.setsTime(time.getTime());
 
                         //converts the date object into epoch
                         // NewTask.setDueDate(date);
@@ -128,6 +138,82 @@ public class AddCourseActivity extends AppCompatActivity  {
         );
     }
 
+    /*
+    This method converts the the codes present in the <var>data<var> <code>ArrayList<code> to
+    its corresponding string value.
+
+    The way this method works is pretty simple. The day-code <code>0<code> stands for Monday and
+    follows along till <code>4<code> denotes Friday. Each individual <code>if<code> statement
+    compares if the respective day-code is present in the <var>data<var> <code>ArrayList<code> and
+    as if present, returns a value greater than 0, else -1. The string fragment is the concatenated
+    with a ', ' and the next fragment. The final string that is returned is the entire string minus
+    the last 2 characters, namely the ', ' present at the end of the last string fragment.
+     */
+    private String DayCodeConverter(ArrayList days) {
+
+        String temp_string = "";
+
+        if (days.indexOf(0) >= 0)
+            temp_string = temp_string + "Monday" + ", ";
+        if (days.indexOf(1) >= 0)
+            temp_string = temp_string + "Tuesday" + ", ";
+        if (days.indexOf(2) >= 0)
+            temp_string = temp_string + "Wednesday" + ", ";
+        if (days.indexOf(3) >= 0)
+            temp_string = temp_string + "Thursday" + ", ";
+        if (days.indexOf(4) >= 0)
+            temp_string = temp_string + "Friday" + ", ";
+
+        try {
+            return temp_string.substring(0, temp_string.length() - 2);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
 
     }
+
+    // This is the interface that communicates with the <class>TimePicker<class> dialog
+    @Override
+    public void onTimeSet(android.widget.TimePicker view, int hour, int minute) {
+        final Calendar c = Calendar.getInstance();
+
+        if(passcode == 1)
+            c.setTime(CourseStartTime);
+        else if(passcode == 2)
+            c.setTime(CourseEndTime);
+
+        c.set(Calendar.HOUR_OF_DAY, hour);
+        c.set(Calendar.MINUTE, minute);
+
+        if(passcode == 1)
+        {
+            CourseStartTime = c.getTime();
+            sTime.setText(timeFormat.format(CourseStartTime));
+        }
+        else if (passcode == 2)
+        {
+            CourseEndTime = c.getTime();
+            eTime.setText(timeFormat.format(CourseEndTime));
+
+        }
+    }
+
+    // Interface that communicates with the <class>DayPicker<class> dialog
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        try {
+            args.getBundle("data");
+            mSelectedDays = args.getStringArrayList("days");
+            passcode = 3;
+        } catch (NullPointerException e) {
+            mSelectedDays = new ArrayList();
+        }
+        if(passcode == 3)
+            day.setText(DayCodeConverter(mSelectedDays));
+    }
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+    }
+}
+
 
